8e311ab33931716acb1ffaf4766a3bb6feaaa23a
Denis Tsukanov <tsukanovdenis83@yandex.ru>
2023-09-24 21:38:16 +0400

    finished Д.З.ЦукановДенис-26.09.2023

--- Д.З.ЦукановДенис-26.09.2023/script.js
+++ Д.З.ЦукановДенис-26.09.2023/script.js
@@ -4,6 +4,13 @@ addEventListener("DOMContentLoaded", (event) => {
     const ctx = field.getContext('2d');
     const ctx2 = field2.getContext('2d');
     const btn = document.querySelector('.btn');
+    const btn2 = document.querySelector('.task-7 .btn');
+    const btn3 = document.querySelector('.task-8 .btn');
+    const btn4 = document.querySelector('.task-9 .btn');
+    const btn5 = document.querySelector('.task-10 .btn');
+    const btn6 = document.querySelector('.task-11 .btn');
+    const btn7 = document.querySelector('.task-12 .btn');
+    const btn8 = document.querySelector('.task-13 .btn');
 
     let widthRect = 0;
     let heightRect = 0;
@@ -152,12 +159,195 @@ addEventListener("DOMContentLoaded", (event) => {
         res.textContent = `${Math.abs(width) * 2 + Math.abs(height) * 2}`;
     }
 
+    setWidth = (rect) => {
+        const inp = +document.querySelector('.task-7 .inp').value;
+        const sel = document.querySelector('.task-7 select').value;
+
+        let x = sel == '-' ? rect[1].x - inp : rect[1].x + inp;
+        document.querySelector('.task-1 .inp.third').value = x;
+        createRect(rect[0].x, rect[0].y, x, rect[1].y);
+    }
+
+    setHeight = (rect) => {
+        const inp = +document.querySelector('.task-8 .inp').value;
+        const sel = document.querySelector('.task-8 select').value;
+
+        let y = sel == '-' ? rect[1].y - inp : rect[1].y + inp;
+
+        document.querySelector('.task-1 .inp.fourth').value = y;
+        createRect(rect[0].x, rect[0].y, rect[1].x, y);
+    }
+    
+    setWidthAndHeight = (rect) => {
+        const inp1 = +document.querySelector('.task-9 .inp.first').value;
+        const sel1 = document.querySelector('.task-9 .sel-1').value;
+        const inp2 = +document.querySelector('.task-9 .inp.second').value;
+        const sel2 = document.querySelector('.task-9 .sel-2').value;
+
+        let width = sel1 == '-' ? rect[1].x - inp1 : rect[1].x + inp1;
+        let height = sel2 == '-' ? rect[1].y - inp2 : rect[1].y + inp2;
+
+        document.querySelector('.task-1 .inp.third').value = width;
+        document.querySelector('.task-1 .inp.fourth').value = height;
+        createRect(rect[0].x, rect[0].y, width, height);
+    }
+
+    moveXAxis = (rect) => {
+        const inp = +document.querySelector('.task-10 .inp').value;
+        const sel = document.querySelector('.task-10 select').value;
+
+        let x1 = sel == '-' ? rect[0].x - inp : rect[0].x + inp;
+        let x2 = sel == '-' ? rect[1].x - inp : rect[1].x + inp;
+
+        document.querySelector('.task-1 .inp.first').value = x1;
+        document.querySelector('.task-1 .inp.third').value = x2;
+        createRect(x1, rect[0].y, x2, rect[1].y);
+    }
+
+    moveYAxis = (rect) => {
+        const inp = +document.querySelector('.task-11 .inp').value;
+        const sel = document.querySelector('.task-11 select').value;
+
+        let y1 = sel == '-' ? rect[0].y - inp : rect[0].y + inp;
+        let y2 = sel == '-' ? rect[1].y - inp : rect[1].y + inp;
+
+        document.querySelector('.task-1 .inp.second').value = y1;
+        document.querySelector('.task-1 .inp.fourth').value = y2;
+        createRect(rect[0].x, y1, rect[1].x, y2);
+    }
+
+    moveXAndYAxises = (rect) => {
+        const inp1 = +document.querySelector('.task-12 .inp.first').value;
+        const sel1 = document.querySelector('.task-12 .sel-1').value;
+        const inp2 = +document.querySelector('.task-12 .inp.second').value;
+        const sel2 = document.querySelector('.task-12 .sel-2').value;
+
+        let x1 = sel1 == '-' ? rect[0].x - inp1 : rect[0].x + inp1;
+        let x2 = sel1 == '-' ? rect[1].x - inp1 : rect[1].x + inp1;
+        let y1 = sel2 == '-' ? rect[0].y - inp2 : rect[0].y + inp2;
+        let y2 = sel2 == '-' ? rect[1].y - inp1 : rect[1].y + inp2;
+
+        document.querySelector('.task-1 .inp.first').value = x1;
+        document.querySelector('.task-1 .inp.third').value = x2;
+
+        document.querySelector('.task-1 .inp.second').value = y1;
+        document.querySelector('.task-1 .inp.fourth').value = y2;
+        createRect(x1, y1, x2, y2);
+    }
+
+    getCoordinatesOfPoint = (rect) => {
+        let x = +document.querySelector('.task-13 .inp.first').value;
+        let y = +document.querySelector('.task-13 .inp.second').value;
+        let res = document.querySelector('.task-13 .res');
+        let x1 = rect[0].x;
+        let y1 = rect[0].y;
+        let x2 = rect[1].x;
+        let y2 = rect[1].y;
+
+        if(x1 < x2 && y1 < y2) {
+            if(x >= x1 && x <= x2 && y >= y1 && y <= y2) {
+                res.textContent = 'Эта точка внутри прямоугольника!';
+            } else {
+                res.textContent = 'Эта точка НЕ внутри прямоугольника!';
+            }
+        }
+
+        if(x1 > x2 && y1 > y2) {
+            if(x >= x2 && x <= x1 && y >= y2 && y <= y1) {
+                res.textContent = 'Эта точка внутри прямоугольника!';
+            } else {
+                res.textContent = 'Эта точка НЕ внутри прямоугольника!';
+            }
+        }
+
+        if(x1 > x2 && y1 < y2) {
+            if(x >= x2 && x <= x1 && y >= y1 && y <= y2) {
+                res.textContent = 'Эта точка внутри прямоугольника!';
+            } else {
+                res.textContent = 'Эта точка НЕ внутри прямоугольника!';
+            }
+        }
+
+        if(x1 < x2 && y1 > y2) {
+            if(x >= x1 && x <= x2 && y >= y2 && y <= y1) {
+                res.textContent = 'Эта точка внутри прямоугольника!';
+            } else {
+                res.textContent = 'Эта точка НЕ внутри прямоугольника!';
+            }
+        }
+
+    }
+    
+    //Создать объект
     btn.onclick = () => {
         const rect = [
             {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
             {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
         ]
         createRect(rect[0].x, rect[0].y, rect[1].x, rect[1].y);
+    }
+
+    //Изменить ширину
+    btn2.onclick = () => {
+        const rect = [
+            {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
+            {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
+        ]
+        setWidth(rect);
+    }
+
+    //Изменить высоту
+    btn3.onclick = () => {
+        const rect = [
+            {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
+            {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
+        ]
+        setHeight(rect);
+    }
+
+    //Изменить ширину и высоту
+    btn4.onclick = () => {
+        const rect = [
+            {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
+            {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
+        ]
+        setWidthAndHeight(rect);
+    }
+
+    //Сместить по оси X
+    btn5.onclick = () => {
+        const rect = [
+            {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
+            {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
+        ]
+        moveXAxis(rect);
+    }
+
+    //Сместить по оси Y
+    btn6.onclick = () => {
+        const rect = [
+            {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
+            {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
+        ]
+        moveYAxis(rect);
+    }
+
+    //Сместить по осям X и Y
+    btn7.onclick = () => {
+        const rect = [
+            {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
+            {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
+        ]
+        moveXAndYAxises(rect);
+    }
+
+    //Определить попадает точка в прямоугольник или нет
+    btn8.onclick = () => {
+        const rect = [
+            {x: +document.querySelector('.first').value, y: +document.querySelector('.second').value},
+            {x: +document.querySelector('.third').value, y: +document.querySelector('.fourth').value}
+        ]
+        getCoordinatesOfPoint(rect);
     }
 });
 
